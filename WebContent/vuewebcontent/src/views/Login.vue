24ebea6e5198e2ce1fbc75df4ef382e11e8d1f24
blandly_ <hanyyzhang@gmail.com>
2024-02-11 21:00:30 +0900

    hanyu
    前端页面部分

--- WebContent/vuewebcontent/src/views/Login.vue
+++ WebContent/vuewebcontent/src/views/Login.vue
@@ -1,4 +1,34 @@
 <template>
-    <input type="text">
-    <input type="password">
+    <h2>Login Form</h2>
+    <div>
+        <div>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username">
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password">
+        </div>
+        <div>
+            <button @click="loginHandler">Login</button>
+        </div>
+    </div>
 </template>
+
+<script>
+    export default {
+        name: "Login",
+        data() {
+            return {
+                username: "",
+                password: ""
+            }
+        },
+        methods: {
+            loginHandler() {
+                console.log("/login to /")
+                this.$router.push({ path:'/'})
+            }
+        }
+    }
+</script>
